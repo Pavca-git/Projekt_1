eec708f8b6b8693f745da756987a733d27bfaafc
Pavca-git <kosturikovap@gmail.com>
2024-07-16 18:47:27 +0200

    vložení na github

---
+++ projekt_1.py
@@ -0,0 +1,103 @@
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+author: Pavla Koštuříková
+email: kosturikovap@gmail.com
+discord: Pavla K
+"""
+
+
+# import textů, ze kterých uživatel vybírá, z listu 'TEXTS'
+from task_template import TEXTS
+
+# registrovaní uživatelé
+# slovník 'users': klíč = jméno uživatele, hodnota = heslo uživatele
+users = {"bob": "123",
+         "ann": "pass123",
+         "mike": "password123",
+         "liz": "pass123"}
+
+# vstup od uživatele - jméno = 'username' a heslo = 'password'
+username = input("username: ")
+password = input("password: ")
+print(odd := "-" * 40)
+
+# kontola správného jména a hesla ze slovníku 'users'
+if users.get(username) != None and users[username] == password:
+    print(f"Welcome to the app, {username}\nWe have 3 texts to be analyzed.")
+else:
+    # v případě neregistovaného uživatele ukončení programu
+    print("Unregistered user, terminating the program...")
+    exit()
+print(odd)
+
+# vstup od uživatele, výběr ze tří textů pomocí čísla 1 až 3,
+# uloženého do proměnné 'num'
+num = input("Enter a number btw. 1 and 3 to select: ")
+print(odd)
+
+# v případě chybného vstupu od uživatele, ukončení programu
+if not num.isdigit():
+    print("Input is not a number, terminating the program...")
+    exit()
+elif int(num) < 1 or int(num) > 3:
+    print("Number is not correct, terminating the program...")
+    exit()
+
+# vybrání konkrétního textu z listu 'TEXTS' a uložení do proměnné 'text'
+# ze stringu 'text' převední do listu 'words'
+num = int(num)
+text = str(TEXTS[num-1])
+words = text.split()
+# print(words)
+
+# 'words_only' = odstranění z listu 'words' . a ,
+words_only = []
+for word in words:
+    if "." in word: 
+        without_dot = word.replace(".","")
+        words_only.append(without_dot)
+    elif "," in word:
+        without_comma = word.replace(",","")
+        words_only.append(without_comma)
+    else:    
+        words_only.append(word)
+# print(words_only)
+
+# 'word_count' - počet slov v listu 'words_only'
+word_count = len(words_only)
+print(f"There are {word_count} words in the selected text.")
+
+title_count = 0     # počet slov v listu 'words_only' začínajících velkým písmenem
+upper_count = 0     # počet slov v listu 'words_only' psaných velkými písmeny
+lower_count = 0     # počet slov v listu 'words_only' psaných malými písmeny
+number_count = 0    # počet čísel v listu 'words_only'
+sum_number = 0      # součet všech čísel v listu 'words_only'
+frequency = []      # četnost délek slov v listu 'word_only'
+
+for word in words_only:
+    if word.isalpha():
+        title_count += int(word.istitle())
+        upper_count += int(word.isupper())
+        lower_count += int(word.islower())
+    number_count += int(word.isnumeric())
+    if word.isnumeric():
+        sum_number += int(word)
+    frequency.append(len(word))
+
+print(f"There are {title_count} titlecase words.")
+print(f"There are {upper_count} uppercase words.")
+print(f"There are {lower_count} lowercase words.")
+print(f"There are {number_count} numeric strings.")
+print(f"There sum of all the numbers {sum_number}.")
+print(odd)
+
+# graf četnosti délek slov v listu 'word_only'
+print("LEN|", "OCCURENCES".center(16), "|NR.")
+print(odd)
+for length in range(1,max(frequency)+1):
+    fr = frequency.count(length)
+    star = "*" * fr
+    if fr:
+        print(f"{str(length).rjust(3)}|{star.ljust(18)}|{str(fr).ljust(0)}")
+
+      
